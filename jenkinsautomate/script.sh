2e0f706a9b32da1b1cebbeb88c055441f408d463
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2024-10-15 13:35:03 +0530

    Update script.sh

--- jenkinsautomate/script.sh
+++ jenkinsautomate/script.sh
@@ -2,6 +2,7 @@
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
 sudo amazon-linux-extras install java-openjdk11 -y
 yum install jenkins -y
 service jenkins start
