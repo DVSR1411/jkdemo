1eb75ee2b4b73b112c62d0cc58fe587b4430c466
DANDA VENKATA SATHWIK REDDY <165892101+DVSR1411@users.noreply.github.com>
2024-12-18 15:11:51 +0530

    Update script.sh

--- jenkinsautomate/script.sh
+++ jenkinsautomate/script.sh
@@ -3,8 +3,14 @@ yum update -y
 wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum upgrade
+yum upgrade -y
 dnf install java-17-amazon-corretto -y
 yum install jenkins -y
+yum install git -y
+yum install docker -y
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+systemctl enable docker
+systemctl start docker
+sudo usermod -aG docker jenkins
 systemctl enable jenkins
 systemctl start jenkins
